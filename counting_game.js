1b55288a5920eb89fbbc53978f595a124b0744ca
Fully34 <cfullinw34@gmail.com>
2015-05-18 11:49:56 -0600

    refactoring to take two inputs

--- counting_game.js
+++ counting_game.js
@@ -12,12 +12,12 @@ increment up to 7, then decrement until we get to count = 14 --> (if count% 7, s
 
 */
 
-function playerArr(num){
+function playerArr(num) {
 
 	var player = [];
 
-	for (var i = 0; i<num; i++){
-		player.push(i+1);
+	for (var i = 0; i < num; i++) {
+		player.push(i + 1);
 	}
 	return player;
 }
@@ -25,43 +25,70 @@ function playerArr(num){
 
 // playerNum(10);
 
-function game(target, playerNum){
+function game(target, playerNum) {
 
 	var player = playerArr(playerNum); //--> returns an array of [1, ..., playerNum]
 	var direction = 1;
 	var index = 0;
 
-	for (var i=1; i<=target; i ++){
+	for (var i = 1; i <= target; i++) {
 
-		if((i%7 === 0)&&(i%11 === 0)&&(i!==0)){
+		if ((i % 7 === 0) && (i !== 0)) {
 			direction = (-1 * direction);
 			// console.log(i);
 			// console.log("skip = " +skip+ ", direction = "+direction);
-		}else if((i%7 === 0 )&&(i!==0)){
-			direction = (-1 * direction);
-			// console.log(i);
-			// console.log("direction = " + direction);
 		}
 
-		if(index < 0){
-			index = 9;
-		}else if (index > 9){
-			index = 0;
-		}else if(((i-1)%11===0)&&((i-1)>0)){
+		if (index < 0) {
+			console.log("index less than zero" + index);
+			if (index === -1){
+				console.log(index);
+				index = (player.length - 1);
+				console.log("index flip");
+				console.log(index);
+			} else if (index === -2){
+				console.log(index);
+				index = (player.length - 2);
+				console.log("mega flip");
+				console.log(index);
+			}
+		} else if (index > (player.length)) {
+			console.log("index greater than length" + index);
+			if (index === (player.length)) {
+				console.log(index);
+				index = 0;
+				console.log("index flip");
+				console.log(index);
+			}else if (index === (player.length + 1)) {
+				console.log(index);
+				index = 1;
+				console.log("mega flip");
+				console.log(index);
+			}
+		} 
+
+		if (((i - 1) % 11 === 0) && ( (i - 1) > 0)) {
 			index += direction;
+			console.log("skip");
+			if (index === -1){
+				console.log("index flip");
+				index = (player.length - 1);
+			} else if (index === -2){
+				console.log("mega flip");
+				index = (player.length - 2);
+			}
 		}
 
 		console.log("player %d says %d | index = %s", player[(index)%player.length], i, index);
 
-		if(i === target){
-			continue;
-		}
-
 		index += direction;
+		if (i % 7 === 0){
+			console.log("switch" + " | " + direction);
+		}
 	}
 }
 
-game(100,10);
+game(147,3);
 
 
 
