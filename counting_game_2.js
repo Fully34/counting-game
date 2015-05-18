051b3c8718d8d38531a8943c5b40eb7cf731d3f0
Fully34 <cfullinw34@gmail.com>
2015-05-18 14:19:05 -0600

    final commit, before Rob help

--- counting_game_2.js
+++ counting_game_2.js
@@ -29,50 +29,54 @@ function game(target, players){
 
 	var array = playerArr(players);
 	var count = 0;
-	var index = -1;
+	var index = -1; //--> NEEDS TO BE NEGATIVE ONE FOR THE VERY FIRST ITERATION
 	var direction = 1
+	
+	if (players < 3){
+		console.log("That's boring, you need more players than that!");
+	}else{
+		for (var i = 0; i < target; i++){
+			
+			count += 1;
 
-
-	for (var i = 0; i < target; i++){
-		
-		count += 1;
-
-		if ((divSeven(count - 1)) && (i !== 0)) {
-			direction = direction * (-1);
-		}
-
-		if ((divEleven(count - 1)) && (i !== 0)) {
-			direction = (direction * 2);
-		}
-
-		index += direction;
-		console.log("index: " + index + " | " + "direction: " + direction);
-
-		if ((divEleven(count - 1)) && (i !== 0)) {
-			direction = direction/2;
-		}
-
-		if(i > 0){
-			if (index < 0) {
-				if (index === -1) {
-					index = (array.length - 1);
-				}else if (index === -2){
-					index = (array.length - 2)
-				}
-			}else if (index > (array.length - 1)) {
-	            if (index === array.length) {
-	            	index = 0;
-	            }else if(index === (array.length + 1)){
-	            	index = 1; 
-	            }
+			if ((divSeven(count - 1)) && (i !== 0)) {
+				direction = direction * (-1);
 			}
+
+			if ((divEleven(count - 1)) && (i !== 0)) {
+				direction = (direction * 2);
+			}
+
+			index += direction;
+			console.log("index: %s | direction: %d | count: %d", index, direction, count);
+
+			if ((divEleven(count - 1)) && (i !== 0)) {
+				direction = direction/2;
+			} // --> Set the direction back to either +/- 1 AFTER incrementing the index
+
+
+			// DEALING WITH SITUATION WHERE INDEX IS EITHER LESS THAN ZERO OR GREATER THAN THE ARRAY LENGTH//
+			//===========================================// 
+			if(i > 0){ // --> Had to do this to prevent modification of the index var on the very first iteration//
+				if (index < 0) {
+					if (index === -1) {
+						index = (array.length - 1);
+					}else if (index === -2){
+						index = (array.length - 2)
+					}
+				}else if (index > (array.length - 1)) {
+		            if (index === array.length) {
+		            	index = 0;
+		            }else if(index === (array.length + 1)){
+		            	index = 1; 
+		            }
+				}
+			}
+
+			console.log("----Player # %s says %d, %s", array[index], count, direction + "----"); // --> USE THIS TO VISUALIZE PROCESS
 		}
-
-		console.log("Player # %s says %d, %d", array[index], count, direction);
 	}
-
-	return "Player # " + array[index] + " says " + target;
-
+	return "Player # " + array[index] + " says " + target; //--> Returns 'player' who says 'target'
 }
 
 game(150, 7);
